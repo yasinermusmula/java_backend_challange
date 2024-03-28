7136ac947b208be35621dcc8bc0fee5b847ee099
Yasin Ermusmula <yasin.ermusmula1@gmail.com>
2024-03-28 10:46:00 +0300

    Ecommerce backend project created

---
+++ s19challange/src/main/java/com/example/s19challange/S19challangeApplication.java
@@ -0,0 +1,13 @@
+package com.example.s19challange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S19challangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S19challangeApplication.class, args);
+    }
+
+}
